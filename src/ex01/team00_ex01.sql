008c73260353d0f60e5de4fd454f676173d62c83
Nikolay Surkov <nikolaysurkov@MacBook-Pro-Nikolay.local>
2023-11-04 01:58:07 +0300

    mod: style

--- src/ex01/team00_ex01.sql
+++ src/ex01/team00_ex01.sql
@@ -20,33 +20,43 @@ VALUES ('a', 'b', 10),
        ('c', 'd', 30);
 
 
-WITH RECURSIVE trip AS (SELECT 1 path_length
-       , 0.0 AS total_cost
-       , ARRAY [n.point1] path
-       , n.point1 last_point
-    FROM nodes n
-   WHERE n.point1 = 'a' UNION
-SELECT t.path_length + 1
-       , t.total_cost + n.cost
-       , t.path || n.point2
-       , point2
-    FROM trip t JOIN
-         nodes n ON t.last_point = n.point1
-   WHERE n.point2 != ALL(t.path) OR(t.path_length = 4
-                                        AND n.point2 = 'a')
- )
-SELECT t.total_cost
-     , t.path tour
-  FROM trip t
- WHERE t.path_length = 5
-   AND(t.total_cost = (SELECT min(t2.total_cost)
-                         FROM trip t2
-                        WHERE t2.path_length = 5
-                      ) OR t.total_cost = (SELECT max(t2.total_cost)
-                                             FROM trip t2
-                                            WHERE t2.path_length = 5
-                                          ))
- ORDER BY 1, 2
-;
+WITH RECURSIVE trip AS (
+   SELECT 1 path_length,
+      0.0 AS total_cost,
+      ARRAY [n.point1] path,
+      n.point1 last_point
+   FROM nodes n
+   WHERE n.point1 = 'a'
+   UNION
+   SELECT t.path_length + 1,
+      t.total_cost + n.cost,
+      t.path || n.point2,
+      point2
+   FROM trip t
+      JOIN nodes n ON t.last_point = n.point1
+   WHERE n.point2 != ALL(t.path)
+      OR(
+         t.path_length = 4
+         AND n.point2 = 'a'
+      )
+)
+SELECT t.total_cost,
+   t.path tour
+FROM trip t
+WHERE t.path_length = 5
+   AND(
+      t.total_cost = (
+         SELECT min(t2.total_cost)
+         FROM trip t2
+         WHERE t2.path_length = 5
+      )
+      OR t.total_cost = (
+         SELECT max(t2.total_cost)
+         FROM trip t2
+         WHERE t2.path_length = 5
+      )
+   )
+ORDER BY 1,
+   2;
 
 DROP TABLE nodes;
