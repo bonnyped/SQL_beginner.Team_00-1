c9157bcfb8ce1d38c2dcdf67b0a22d9ac6e2a22e
bonnyped <bonnyped@student.21-school.ru>
2023-11-04 04:27:10 +0600

    final ex01

--- src/ex01/team00_ex01.sql
+++ src/ex01/team00_ex01.sql
@@ -1,11 +1,11 @@
 CREATE TABLE nodes
 (
-    point_1 char,
-    point_2 char,
-    cost    int
+    point1 varchar NOT NULL,
+    point2 varchar NOT NULL,
+    cost   numeric NOT NULL DEFAULT 0.0
 );
 
-INSERT INTO nodes (point_1, point_2, cost)
+INSERT INTO nodes (point1, point2, cost)
 VALUES ('a', 'b', 10),
        ('b', 'a', 10),
        ('a', 'c', 15),
@@ -19,51 +19,34 @@ VALUES ('a', 'b', 10),
        ('d', 'c', 30),
        ('c', 'd', 30);
 
-WITH RECURSIVE trip AS
-                   (SELECT 1                       path_length,
-                           0 AS                    total_cost,
-                           ARRAY [n.point_1::bpchar] path,
-                           n.point_1                 last_point
-                    FROM nodes n
-                    WHERE n.point_1 = 'a'
-                    UNION
-                    SELECT t.path_length + 1,
-                           t.total_cost + n.cost,
-                           t.path || n.point_2,
-                           point_2
-                    FROM trip t
-                             JOIN nodes n ON t.last_point = n.point_1
-                    WHERE n.point_2 != ALL (t.path)
-                       OR (t.path_length = 4 AND n.point_2 = 'a')
-                   )
-SELECT t.total_cost,
-       t.path::varchar tour
-FROM trip t
-WHERE t.path_length = 5
-  AND (t.total_cost =
-       (SELECT min(t2.total_cost)
-        FROM trip t2
-        WHERE t2.path_length = 5)
-    OR t.total_cost = (SELECT max(t2.total_cost)
-                        FROM trip t2
-                        WHERE t2.path_length = 5))
-ORDER BY 1, 2;
+
+WITH RECURSIVE trip AS (SELECT 1 path_length
+       , 0.0 AS total_cost
+       , ARRAY [n.point1::bpchar] path
+       , n.point1 last_point
+    FROM nodes n
+   WHERE n.point1 = 'a' UNION
+SELECT t.path_length + 1
+       , t.total_cost + n.cost
+       , t.path || n.point2
+       , point2
+    FROM trip t JOIN
+         nodes n ON t.last_point = n.point1
+   WHERE n.point2 != ALL(t.path) OR(t.path_length = 4
+                                        AND n.point2 = 'a')
+ )
+SELECT t.total_cost
+     , t.path::varchar tour
+  FROM trip t
+ WHERE t.path_length = 5
+   AND(t.total_cost = (SELECT min(t2.total_cost)
+                         FROM trip t2
+                        WHERE t2.path_length = 5
+                      ) OR t.total_cost = (SELECT max(t2.total_cost)
+                                             FROM trip t2
+                                            WHERE t2.path_length = 5
+                                          ))
+ ORDER BY 1, 2
+;
 
 DROP TABLE nodes;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
